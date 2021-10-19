09a0124d9afe549e76f7eaf173ddcd021e5dd662
faizazafar <57325353+faizazafar@users.noreply.github.com>
2021-10-19 15:57:25 +0500

    Add files via upload

---
+++ Reactassignment3/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Aboutus from './components/Aboutus'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Howitwork from './components/Howitwork'
+import Navbar from './components/Navbar'
+import Navbarr from './components/Navbarr'
+
+export default function App() {
+  return (
+    <div>
+      {/* <Navbarr></Navbarr>
+      <Header></Header>
+      <Howitwork> </Howitwork>
+      <Aboutus></Aboutus> */}
+      {/* <Stateclass></Stateclass> */}
+      
+
+    </div>
+  )
+}
